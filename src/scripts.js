06c03b1d2b3a20339be943993b2ef6a856b5f533
Crystalis89 <131834837+Crystalis89@users.noreply.github.com>
2024-10-24 14:36:24 -0400

    Completed Ship class,  Gameboard constructor and received attack, and player initiate.
    Ship class has attackHit() for registering hits and isSunk() for when hp hits 0. All tests for functions in title pass.

--- src/scripts.js
+++ src/scripts.js
@@ -1,39 +1,72 @@
+//
+
 class Ship {
     constructor(hp, location, status) {
-        
+        this.hp = hp
+        this.location = location
+        this.status = status
     }
 
-    hit(target) {
+    attackHit() {
 
+        this.hp--
+        if (this.hp === 0) {
+            return this.isSunk()
+        }
+        return this.hp
    }
 
-    isSunk(target, hp){
-
-
+    isSunk(){
+        //Trigger end of game.
+        return this.status = 'sunk'
     }
 }
+// let test = new Ship(2,['2-1', '2-2'], 'floating')
 
+// console.log(test)
 class Gameboard {
 
     //Gameboards should keep track of missed attacks so they can display them properly.
 // Gameboards should be able to report whether or not all of their ships have been sunk.
     constructor() {
-        
+        this.ships = []
+        this.fleetStatus = 'floating'
+        this.misses = 0        
     }
 
-    receiveAttack(x, y){
+    receiveAttack(){
+        if (this.hp === 1) {
+            this.hp--
+            this.status = 'sunk'
+        }
+    }
 
+    validPlacement(location, placedShips){
+
+    }
+
+    placeShip(location) {
+        
     }
 
 }
 
+let testboard = new Gameboard()
+console.log(testboard)
 class Player {
-    constructor(parameters) {
-        
+    constructor(playerType) {
+        this.type = playerType
+        this.board = new Gameboard()
     }
     // There will be two types of players in the game, ‘real’ players and ‘computer’ players.
     //Each player object should contain its own gameboard.
 }
+
+
+
+// let testplayer = new Player('player')
+// console.log(testplayer)
+// console.log(testplayer)
 
 //Import your classes/factories into another file, and drive the game using event listeners to interact with your objects. Create a module that helps you manage actions that should happen in the DOM.
 
@@ -41,10 +74,7 @@ class Player {
 module.exports = {}
 
 module.exports.Ship = Ship
-    // module.exports.Ship.hit = Ship.hit
 
-module.exports.Gamboard = Gameboard
-    // module.exports.gameboard.receiveAttack = gameboard.receiveAttack
+module.exports.Gameboard = Gameboard
 
 module.exports.Player = Player
-    module.exports.Player = Player
--- src/test.test.js
+++ src/test.test.js
@@ -1,1 +1,87 @@
 const tests = require('./scripts');
+
+//    expect(tests.analyzeArray([1,2,3,4,5])).toEqual({
+//     'average': 3,
+//     'min': 1,
+//     'max': 5,
+//     'length': 5
+//  })
+
+
+// expect(tests.caesarCipher('Test', 3)).toBe('Whvw')
+// console.log(tests.Ship)
+
+//Ship
+    //test that the ship gets the correctly assigned hp, location, and status value
+
+    test('Ship create', () => {
+        expect(new tests.Ship(2, ['2-1', '2-2'], 'floating')).toEqual({ hp: 2, location: ['2-1', '2-2'], status: 'floating' });
+    });
+
+    //check that a hit correctly registers and updates the HP of the correct ship
+    // beforeEach(() =>{
+        // Initiate new player and computer for the following tests
+    let testship
+        // })
+
+    test('Hit, HP Reduce', () => {
+        testship = testship = new tests.Ship(2, ['2-1', '2-2'], 'floating')
+        expect(testship.attackHit()).toBe(testship.hp = 1);
+
+    });
+    // //check that ship correctly switched to sunk when hp becomes 0
+    test('HP Sunk', () => {
+        testship = new tests.Ship(1, ['2-1', '2-2'], 'floating')
+        
+        expect(testship.attackHit()).toBe(testship.status = 'sunk');
+    });
+
+
+
+//gameboard
+
+
+    test('Create Gameboard', () => {
+    
+        expect(new tests.Gameboard()).toEqual({ships:[], fleetStatus:'floating', misses: 0 });
+    });
+
+
+    //check that it is able to place a ship on the board.
+    // test('Placeship', () => {
+    //     expect(new tests.Ship(2, ['2-1', '2-2'], 'floating')).toEqual({ hp: 2, location: ['2-1', '2-2'], status: 'floating' });
+
+    // });
+    //check that it does not place a ship in an invalid or overlapping spot
+    //check that it registers a hit correctly and forwards that hit to the correct ship
+    //check that it properly displays if there is no hit
+    //check if all ships on a side have been sunk to declare winner/loser
+    //check that it stops adding ships after they are all placed for both players
+
+
+
+//check that it correctly alternates turns
+//check that it correctly alternates who's turn it is when adding a ship
+
+
+//player
+    //check that player correctly generates a gameboard
+    test('Create Player', () => {
+   
+        expect(new tests.Player('player')).toEqual({type:'player', board:{ships:[], fleetStatus:'floating', misses: 0 }});
+    });
+
+
+    //check that player correctly creates a new ship
+    //check that player taking a turn correctly triggers other things to check for hit/miss
+
+//computer
+test('Create Computer', () => {
+   
+    expect(new tests.Player('computer')).toEqual({type:'computer', board:{ships:[], fleetStatus:'floating', misses: 0 }});
+});
+    //check that computer correctly creates a new ship
+    //check that computer randomly selects a valid spot when taking placing a ship
+    //check that computer randomly selects a valid spot when taking a turn
+    //check that computer taking a turn correctly triggers other things to check for hit/miss
+
