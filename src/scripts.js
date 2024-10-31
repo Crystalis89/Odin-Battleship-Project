a8a2edd8524c27f000ed8bb8c8ab86d2509effdf
Crystalis89 <131834837+Crystalis89@users.noreply.github.com>
2024-10-30 21:46:00 -0400

    All parts of receive attack complete, more validations in validPlacement
    Taking coordinates for the attack checks whether to many targets,  if it is a miss or not, if it is a hit will reduce hp of the ship if not increment miss on the gameboard. If HP is 0 will mark ship as sunk. If all are sunk game will be over.
    
    Still need to add tracking for what has already been hit so cannot hit same spot twice.

--- src/scripts.js
+++ src/scripts.js
@@ -10,10 +10,11 @@ class Ship {
     attackHit() {
 
         this.hp--
+        // console.log(this.hp)
         if (this.hp === 0) {
-            return this.isSunk()
+            this.isSunk()
         }
-        return this.hp
+        return this
    }
 
     isSunk(){
@@ -34,37 +35,94 @@ class Gameboard {
         this.misses = 0        
     }
 
-    receiveAttack(){
+    receiveAttack(target){
+        let targetship
         //Check if the attack matches any of the currently unhit targets. If so use the attackHit method of whichever ship it is. If it misses increment this.misses. If all ships dead declare win/lose
         //check which ship by comparing the location of each ship with the attack, in which case this.ships[shipindex].attackHit
+        // console.table(this.ships)
 
-    }
-
-    validPlacement(location, hp){
-        if (location.length > 3) {
+        //Check whether to many attack targets.
+        if (target.length > 1) {
+            // console.log("To many targets")
             return false
         }
 
-        if (this.ships.length < 5) {
+        //Increment through ships on gameboard to compare against the coordinates targeted
+        for (const ship of this.ships) {
+            //Get the location of the ship
+            for (const loccheck of ship.location) {
+                //Compare the location to up to three coordinates
+                if (loccheck === target[0] || loccheck === target[1] || loccheck === target[2] ) {
 
-            let split
-            //Check if overlaps with any location of another ship. If not split the coordinates in each location, check if any of them are below 0 or above 9. If valid createa a new Ship
+                    //If it hits call this which decrements the hp by 1
+                    ship.attackHit()
 
-            //Iterate through the array of location coordinate arrays
-            for (const locationnode of location) {
-                
-                //Iterate through coordinates in each array.
-                for (const coord of locationnode) {
-                    split = coord.split('-')
-                
-                    //Check if any of the coordinates are to high or low
-                    if (parseInt(split[0]) > 9 || parseInt(split[0]) < 0 || parseInt(split[1]) > 9 || parseInt(split[1]) < 0){
-                        console.log('Out of bounds')
-                        return false                                    
-                    }                
+                    // console.log(ship.status)
+                    // console.log(ship.hp)
+
+                    // //If the ship was changed to sunk after the hit go down this path.
+                    if (ship.status === 'sunk') {
+                        // console.log(ship.hp)
+                        // console.log(ship.status)
+
+                        let losscheck = this.ships.length
+                        // console.log(losscheck)
+                        //Loop through array of ships to see if they all are sunk. If so end the game.
+                        for (const shipcheck of this.ships) {
+
+                            if (shipcheck.status === 'sunk') {
+                                losscheck--
+                            }
+                        }
+
+                        //When create the game driver/listen add if gameboard.fleetStatus = 'sunk 'after recieveAttack is run then end the game.
+
+                        if (losscheck === 0) {
+                            // console.log('Fleet is sunk')
+                            this.fleetStatus = 'fleetsunk'
+                            return  this.fleetStatus
+                        }
+                    }
+                    return this.ships
+
                 }
-                // console.log(this)
+            }
+            // ship.location
+        }
+        
+        this.misses++
+        return this
+    }
 
+    validPlacement(location){
+        if (location.length > 3) {
+            return false
+        }
+        // console.table(this.ships)
+        if (this.ships.length <= 5) {
+
+            let split = []
+
+            for (const tosplit of location) {
+                split.push(tosplit.split('-'))
+                
+            }
+
+            //Check if overlaps with any location of another ship. If not split the coordinates in each location, check if any of them are below 0 or above 9. If valid createa a new Ship
+            for (const coord of split) {
+              
+                //Check if any of the coordinates are to high or low
+                if (parseInt(coord[0]) > 9 || parseInt(coord[0]) < 0 || parseInt(coord[1]) > 9 || parseInt(coord[1]) < 0){
+                    // console.log('Out of bounds')
+                    return false                                    
+                }                
+            }
+            //Iterate through the array of location coordinate arrays
+            // console.table(split)
+            for (const locationnode of location) {
+   
+                //Iterate through coordinates in each array.
+           
                 //If there are ships already placed in the gameboard
                 if (this.ships.length > 0) {
 
@@ -75,29 +133,34 @@ class Gameboard {
                         for (let i = 0; i < ship.location.length; i++) {
                    
                             if (ship.location[i] === location[0] || ship.location[i] === location[1] || ship.location[i] === location[2] || ship.location[i] === location[3] || ship.location[i] === location[4] || ship.location[i] === location[5]) {
-                                console.log('A ship is already placed there')
+                                // console.log('A ship is already placed there')
                                 return false
                         }                            
                         }
                       
                 }  
-                }
-            
+            }
             
         
-            this.ships.push(new Ship(hp, location, 'floating'))
-
-            return this.ships
+  
 
         }
-    }
-        console.log('To many ships.')
+    } 
+    else {
+        // console.log('To many ships.')
         return false
+    }
+    // console.log(this)
+
+    this.ships.push(new Ship(location.length, location, 'floating'))
+    // console.log(this)
+
+    return this.ships
     }
 
 }
 
-let testboard = new Gameboard()
+// let testboard = new Gameboard()
 // console.log(testboard)
 class Player {
     constructor(playerType) {
--- src/test.test.js
+++ src/test.test.js
@@ -1,4 +1,6 @@
 const tests = require('./scripts');
+let newplayer
+let testship
 
 //Ship
     //test that the ship gets the correctly assigned hp, location, and status value
@@ -8,18 +10,17 @@ const tests = require('./scripts');
     });
 
     //check that a hit correctly registers and updates the HP of the correct ship
-    let testship
 
     test('Hit, HP Reduce', () => {
         testship = testship = new tests.Ship(2, ['2-1', '2-2'], 'floating')
-        expect(testship.attackHit()).toBe(testship.hp = 1);
+        expect(testship.attackHit()).toEqual({ hp: 1, location: ['2-1', '2-2'], status: 'floating' });
 
     });
     // //check that ship correctly switched to sunk when hp becomes 0
     test('HP Sunk', () => {
         testship = new tests.Ship(1, ['2-1', '2-2'], 'floating')
         
-        expect(testship.attackHit()).toBe(testship.status = 'sunk');
+        expect(testship.attackHit()).toEqual({ hp: 0, location: ['2-1', '2-2'], status: 'sunk' });
     });
 
 
@@ -35,34 +36,140 @@ const tests = require('./scripts');
     //check that it is able to place a ship on the board.
     test('Place ship', () => {
     
-        expect(new tests.Gameboard().validPlacement(['2-1', '2-2'], 3)).toEqual([{ hp: 3, location: ['2-1', '2-2'], status: 'floating' }]);
+        expect(new tests.Gameboard().validPlacement(['2-1', '2-2'])).toEqual([{ hp: 2, location: ['2-1', '2-2'], status: 'floating' }]);
     });
+
+    //Check that can add a second ship
+    test('Place second ship', () => {
+       
+        newplayer = new tests.Player('player')
+
+        newplayer.board.validPlacement(['2-1', '2-2'])
+
+    
+
+        expect(newplayer.board.validPlacement(['3-1', '3-2'])).toEqual([{hp: 2, location: ['2-1', '2-2'], status: 'floating'}, {hp: 2, location: ['3-1', '3-2'], status: 'floating'}]);
+
+    });
+
+        //Check that can add a third ship
+        test('Place third ship', () => {
+            newplayer = new tests.Player('player')
+
+            newplayer.board.validPlacement(['2-1', '2-2'])
+            newplayer.board.validPlacement(['3-1', '3-2'])
+         
+        
+    
+            expect(newplayer.board.validPlacement(['4-1', '4-2'])).toEqual([{hp: 2, location: ['2-1', '2-2'], status: 'floating'}, {hp: 2, location: ['3-1', '3-2'], status: 'floating'}, {hp: 2, location: ['4-1', '4-2'], status: 'floating'}]);
+        });
+
+            //Check that can add a fourth ship
+    test('Place fourth ship', () => {
+        newplayer = new tests.Player('player')
+
+        newplayer.board.validPlacement(['2-1', '2-2'])
+        newplayer.board.validPlacement(['3-1', '3-2'])
+        newplayer.board.validPlacement(['5-1', '5-2'])
+ 
+    
+
+        expect(newplayer.board.validPlacement(['4-1', '4-2'])).toEqual([{hp: 2, location: ['2-1', '2-2'], status: 'floating'}, {hp: 2, location: ['3-1', '3-2'], status: 'floating'}, {hp: 2, location: ['5-1', '5-2'], status: 'floating'}, {hp: 2, location: ['4-1', '4-2'], status: 'floating'}]);
+    });
+
+        //Check that can add a fifth ship
+        test('Place fifth ship', () => {
+            newplayer = new tests.Player('player')
+
+            newplayer.board.validPlacement(['2-1', '2-2'])
+            newplayer.board.validPlacement(['3-1', '3-2'])
+            newplayer.board.validPlacement(['5-1', '5-2'])
+            newplayer.board.validPlacement(['6-1', '6-2'])        
+    
+            expect(newplayer.board.validPlacement(['4-1', '4-2'])).toEqual([
+                {hp: 2, location: ['2-1', '2-2'],status: 'floating'}, 
+                {hp: 2, location: ['3-1', '3-2'], status: 'floating'}, 
+                {hp: 2, location: ['5-1', '5-2'], status: 'floating'}, 
+                {hp: 2, location: ['6-1', '6-2'], status: 'floating'}, 
+                {hp: 2, location: ['4-1', '4-2'], status: 'floating'}]);
+        });
+
+            //Check that cannot add a sixth ship
+    // test('Cannot add sixth ship', () => {
+    //     newplayer = new tests.Player('player')
+
+    //     newplayer.board.validPlacement(['2-1', '2-2'])
+    //     newplayer.board.validPlacement(['3-1', '3-2'])
+    //     newplayer.board.validPlacement(['5-1', '5-2'])
+    //     newplayer.board.validPlacement(['6-1', '6-2'])
+    //     newplayer.board.validPlacement(['8-1', '8-2'])
+    //     console.table(newplayer.board.ships.length)
+
+    //     expect(newplayer.board.validPlacement(['4-1', '4-2'])).toEqual(false);
+
+    // });
+
     //Check that returns false if give it more than three coordinates.
     test('Number of coordinates check', () => {
     
-        expect(new tests.Gameboard().validPlacement(['2-1', '2-2', '2-3', '2-4'], 3)).toEqual(false);
+        expect(new tests.Gameboard().validPlacement(['2-1', '2-2', '2-3', '2-4'])).toEqual(false);
     });
 
-    //check that it does not place a ship in an invalid or overlapping spot
+    //check that it does not place a ship in an overlapping spot
     test('Invalid Placement', () => {
-       let newplayer = new tests.Player('player')
-//        console.log (newplayer)
+        newplayer = new tests.Player('player')
 
-// console.log (newplayer.board)
-newplayer.board.validPlacement(['2-1', '2-2'])
-// console.log(newplayer.board)
+        newplayer.board.validPlacement(['2-1', '2-2'])
 
-        expect(newplayer.board.validPlacement(['2-1', '2-2'], 3)).toEqual(false);
+        expect(newplayer.board.validPlacement(['2-1', '2-2'])).toEqual(false);
     });
+
+    //check that placement coord not to high or low.
+    test('Check if coordinate within bounds', () => {
+    
+        expect(new tests.Gameboard().validPlacement(['2-10', '0-2'])).toEqual(false);
+    });
+
 
     //check that it registers a hit correctly and forwards that hit to the correct ship
-    //check that it properly displays if there is no hit and increments the miss value of the right ship
+    test('Recieve attack', () => {
+        newplayer = new tests.Player('player')
+        newplayer.board.validPlacement(['2-1', '2-2'])
+        newplayer.board.validPlacement(['5-1', '5-2'])
+
+        // console.table(newplayer.board.ships)
+
+        expect(newplayer.board.receiveAttack(['2-1'])).toEqual([{ hp: 1, location: ['2-1', '2-2'], status: 'floating' }, { hp: 2, location: ['5-1', '5-2'], status: 'floating' }]);
+    });
+    //Check that it errors if give it to many targets for attack at once.
+    test('To many attack targets', () => {
+        newplayer = new tests.Player('player')
+        newplayer.board.validPlacement(['2-1', '2-2'])
+
+        expect(newplayer.board.receiveAttack(['2-1', '2-2'])).toEqual(false);
+    });
+
+    // check that it properly increments the misses value of the right gameboard
+    test('Attack misses', () => {
+        newplayer = new tests.Player('player')
+        newplayer.board.validPlacement(['2-1', '2-2'])
+
+        expect(newplayer.board.receiveAttack(['2-3'])).toEqual({ships:[{ hp: 2, location: ['2-1', '2-2'], status: 'floating' }], fleetStatus:'floating', misses: 1 });
+        // console.table(newplayer.board.ships)
+    });
+
     //check if all ships on a side have been sunk to declare winner/loser
-    //check that it stops adding ships after they are all placed for both players
+    test('Lose check', () => {
+        newplayer = new tests.Player('player')
+        newplayer.board.validPlacement(['2-1', '2-2', '2-3'])
+        newplayer.board.receiveAttack(['2-1'])
+        newplayer.board.receiveAttack(['2-2'])
+    
+        expect(newplayer.board.receiveAttack(['2-3'])).toEqual('fleetsunk');
 
-//Check that it correctly alternates turns
+    });
 
-
+    
 //player
     //check that player correctly generates a gameboard
     test('Create Player', () => {
@@ -72,6 +179,12 @@ newplayer.board.validPlacement(['2-1', '2-2'])
 
 
     //check that player correctly creates a new ship
+    test('Add ships to player', () => {
+        newplayer = new tests.Player('player')
+
+
+        expect(newplayer.board.validPlacement(['2-1', '2-2'])).toEqual([{ hp: 2, location: ['2-1', '2-2'], status: 'floating' }])
+    });
 
 
 //computer
@@ -79,8 +192,28 @@ newplayer.board.validPlacement(['2-1', '2-2'])
     
         expect(new tests.Player('computer')).toEqual({type:'computer', board:{ships:[], fleetStatus:'floating', misses: 0 }});
     });
+     //check that computer randomly selects a valid spot when taking placing a ship
+
+
     //check that computer correctly creates a new ship
-    //check that computer randomly selects a valid spot when taking placing a ship
-    //check that computer randomly selects a valid spot when taking a turn
+    //Still need to add random coordinate selection by computer
+    // test('Add ships to computer', () => {
+    //     newplayer = new tests.Player('computer')
+
+
+    //     expect(newplayer.board.validPlacement(['2-1', '2-2'])).toEqual([{ hp: 2, location: ['2-1', '2-2'], status: 'floating' }])
+    // });
+
+   
+    //check that computer randomly selects a valid target when taking a turn
+
+
+    
     //check that computer taking a turn correctly triggers other things to check for hit/miss
 
+
+
+
+//check that it stops adding ships after they are all placed for both players and begins the game
+
+//Check that it correctly alternates turns
